917641f27cfe1e14a552fec9774af4a68052f265
Maxrudyi <78867107+Maxrudyi@users.noreply.github.com>
2025-07-31 12:24:51 +0200

    Update ig.js

--- netlify/functions/ig.js
+++ netlify/functions/ig.js
@@ -1,32 +1,36 @@
+// netlify/functions/ig.js
+
 export async function handler(event, context) {
   const token = process.env.IG_TOKEN;
-  const igUserId = process.env.IG_USER_ID || "17841458100536914";
+  const igUserId = process.env.IG_USER_ID;
   const fields = "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp";
   const limit = event.queryStringParameters?.limit || 6;
 
-  const url = `https://graph.facebook.com/v18.0/${igUserId}/media?fields=${fields}&access_token=${token}&limit=25`;
+  const url = https://graph.facebook.com/v18.0/${igUserId}/media?fields=${fields}&access_token=${token};
 
   try {
     const response = await fetch(url);
     const json = await response.json();
+    let data = json.data || [];
 
-    if (!json.data) {
+    if (data.length === 0) {
       throw new Error("No media returned from Instagram API");
     }
 
     // Сортуємо за датою
-    const sorted = json.data.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+    data.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
 
-    // Беремо тільки потрібну кількість
-    const sliced = sorted.slice(0, limit);
+    // Віддаємо тільки потрібну кількість
+    const limited = data.slice(0, limit);
 
     return {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin": "*"
       },
-      body: JSON.stringify(sliced)
+      body: JSON.stringify(limited)
     };
+
   } catch (e) {
     return {
       statusCode: 500,
